66951cf5fe548c9904b03ba394a5ebf07d162c85
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2022-05-23 21:21:52 +0700

    Initial Commit

---
+++ OnTheMap/OnTheMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnTheMapApp.swift
+//  OnTheMap
+//
+//  Created by Ahmad Rafi Wirana on 23/05/22.
+//
+
+import SwiftUI
+
+@main
+struct OnTheMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
